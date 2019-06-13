6671ede2e1a630b0522b8aaf5183e85420ad3f23
xieyalong <123456>
2019-06-13 11:42:14 +0800

    flutter_webview_plugin插件webView

---
+++ lib/flutter/WebViewPage.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
+class  WebViewPage extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    return new WebViewPageState();
+  }
+}
+class WebViewPageState extends State<WebViewPage>{
+  @override
+  Widget build(BuildContext context) {
+       return new WebviewScaffold(
+        url: "https://www.baidu.com/",
+         //设置appBar
+         appBar: new AppBar(
+           title: new Text("Widget webview"),
+         ),
+         scrollBar: true,
+
+    );
+  }
+
+}
