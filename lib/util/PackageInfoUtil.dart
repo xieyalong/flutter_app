adae0ba5e5b0d876d31ac72ba8ae848ecc577313
xieyalong <123456>
2019-06-17 16:25:07 +0800

    版本号

--- lib/util/PackageInfoUtil.dart
+++ lib/util/PackageInfoUtil.dart
@@ -12,6 +12,7 @@ class PackageInfoUtil{
     }
     return _instance;
   }
+  //直接获取，需要提前实例化，第一次使用getAppName()会报错
   void init()async{
     packageInfo=await PackageInfo.fromPlatform();
   }
@@ -32,6 +33,7 @@ class PackageInfoUtil{
     String buildNumber = packageInfo.buildNumber;
     return buildNumber;
   }
+  //异步获取
   void a(){
     PackageInfo.fromPlatform().then((PackageInfo packageInfo) {
       print('>]-----');
